274a53be33a7e73435cbffab955edd9e73044d4a
HeShaoZe <EX-HESHAOZE001>
2019-04-10 23:49:47 +0800

    添加半圆形进度条。

---
+++ WKWebViewJSAndOC/SCDCredPointsHeaderPropressView.h
@@ -0,0 +1,21 @@
+//
+//  SCDCredPointsHeaderPropressView.h
+//  SmartCityDemo2-DEV
+//
+//  Created by 何少泽(总行零售网络金融) on 2018/12/27.
+//  Copyright © 2018年 平安银行-智慧城市项目组. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SCDCredPointsHeaderPropressView : UIView
+
+@property (nonatomic,assign) CGFloat percent;
+
+@property (nonatomic, assign) NSInteger cardPoint;
+
+@end
+
+NS_ASSUME_NONNULL_END
